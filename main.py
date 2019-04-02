a05fbbf1308f79749c93dd559b67c9020276e058
Anton Danylenko <adanylenko@stuy.edu>
2019-04-01 21:53:05 -0400

    s

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ cz = 0
 r = 5
 height = 0
 while height<=150:
-    add_torus(bowl, cx, cy+height, cz, 5, r, 0.01)
+    add_torus(bowl, cx, cy+height, cz, 5, r, 0.20)
     r+=5
     height=int((r**4)/9000000.0)
 fruit = []
-add_sphere(fruit, 250, 205, 0, 50, 0.01)
+add_sphere(fruit, 250, 205, 0, 50, 0.20)
 add_box(bowl, 245, -100, -5, 10, 250, 10)
 parse_file('script2', fruit, transform, screen2, color3)
 parse_file('script2', bowl, transform, screen2, color2)
